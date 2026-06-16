5c44043a29d2bc42d7a87a5a49ca4682b95eea9f
Vivek Harish Banthiya <111743178+VivekHBanthiya@users.noreply.github.com>
2026-06-17 00:37:47 +0530

    Time: 4 ms (76.47%) | Memory: 55.2 MB (58.82%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -1,24 +1,22 @@
 class Solution {
     public String processStr(String s) {
-        int n = s.length();
-        StringBuilder sb = new StringBuilder();
+        StringBuilder str = new StringBuilder();
 
-        for(int i=0; i<n; i++){
-            char c = s.charAt(i);
-            if(c>='a' && c<='z'){
-                sb.append(c);
-            }
-            else if(c=='*' && sb.length()>=1){
-                sb.deleteCharAt(sb.length()-1);
-            }
-            else if(c=='#' && sb.length()>=1){
-                sb.append(sb.toString());
-            }else if(c=='%' && sb.length()>=1){
-                String res = sb.reverse().toString();
-                sb.delete(0, sb.length());
-                sb.append(res);
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '#'){
+                str.append(str.toString());
+            }else if(ch == '%'){
+                str.reverse();
+            }else if(ch == '*'){
+                if(str.length() > 0){
+                    str.deleteCharAt(str.length() - 1);
+                }
+            }else{
+                str.append(ch);
             }
         }
-        return sb.toString();
+        return str.toString();
+
     }
 }
